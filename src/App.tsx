6cac913a0c1772d7078deb05332c102d57e47c65
Osamah Ahmad <104696330+osamahahmad@users.noreply.github.com>
2025-05-15 21:28:16 +0100

    use chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Demo } from './assets/Demo'
+
+function App() {
+
+  return (
+    <Demo />
+  )
+}
+
+export default App
---
+++ src/assets/Demo.tsx
@@ -0,0 +1,10 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+export const Demo = () => {
+    return (
+        <HStack>
+            <Button>Click me</Button>
+            <Button>Click me</Button>
+        </HStack>
+    )
+}
